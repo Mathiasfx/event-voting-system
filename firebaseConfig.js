bd53f4b701342aca5422311842fd2d87b11fe368
pi-mathiasdev <mathiaspereiradev@gmail.com>
2024-11-28 18:06:53 -0300

    [UPDATE] Firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB9s-tAtUM-Ayhe1lJRfax_jjVXJvqGlXI",
-  authDomain: "event-voting-system-789a9.firebaseapp.com",
-  projectId: "event-voting-system-789a9",
-  storageBucket: "event-voting-system-789a9.firebasestorage.app",
-  messagingSenderId: "761250730096",
-  appId: "1:761250730096:web:b8031cbe3415aa8980e4b5",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
